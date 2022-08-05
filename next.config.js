080440afdfaf4dc63bf693544676ae99378b35a0
Pab-lo <salaberry.pablo@gmail.com>
2022-08-05 10:55:46 +0200

    f

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  webpack: (nextConfig, { isServer }) => {
-      if (!isServer) {
-          // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-          config.resolve.fallback = {
-              fs: false
-          }
-      }
-
-      return nextConfig;
-  }
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
+
+module.exports = nextConfig
+
